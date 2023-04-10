3befd3db426e4f741b5b2c7115cb41783e716df0
juan pablo guzman moreno <juanpablo-gm@hotmail.com>
2023-04-09 22:31:39 -0500

    learning styles in react native

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,23 +1,26 @@
-import { View, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+import StyledText from "./StyledText";
 
 const RepositoryItem = (props) => {
   return (
-    <View
-      key={props.id}
-      style={{ padding: 20, paddingBottom: 5, paddingTop: 5 }}
-    >
-      <Text style={{ fontWeight: "bold", marginBottom: 5 }}>
-        id: {props.id}
-      </Text>
-      <Text>FullName: {props.fullName}</Text>
-      <Text>Description: {props.description}</Text>
-      <Text>Language: {props.language}</Text>
-      <Text>Stars: {props.stargazersCount}</Text>
-      <Text>Forks: {props.forksCount}</Text>
-      <Text>Review: {props.reviewCount}</Text>
-      <Text>Rating: {props.ratingAverage}</Text>
+    <View key={props.id} style={styles.container}>
+      <StyledText fontSize='subheading' fontWeight='bold'>FullName: {props.fullName}</StyledText>
+      <StyledText >{props.description}</StyledText>
+      <StyledText >{props.language}</StyledText>
+      <StyledText >Stars: {props.stargazersCount}</StyledText>
+      <StyledText >Forks: {props.forksCount}</StyledText>
+      <StyledText >Review: {props.reviewCount}</StyledText>
+      <StyledText >Rating: {props.ratingAverage}</StyledText>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    paddingBottom: 5,
+    paddingTop: 5
+  }
+})
 
 export default RepositoryItem;
